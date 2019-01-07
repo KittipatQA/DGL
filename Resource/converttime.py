938ebcfc1ecea189e1ebb342f4fe01e24e54e54b
KittipatQA <42168713+KittipatQA@users.noreply.github.com>
2019-01-07 14:22:16 +0700

    goasdasd

--- Resource/converttime.py
+++ Resource/converttime.py
@@ -1,7 +1,7 @@
 ﻿import timestring
 from datetime import datetime
 
-def convert_time(date_input):
+def convert_data_time(date_input):
     test = timestring.Date(date_input)
     test_str = str(test)
     dt_str = datetime.strptime(test_str, '%Y-%m-%d %H:%M:%S')
